ac845ade78358401bd41885eedaa78f0a3036f8b
VIMALCHINTU <158822830+VIMALCHINTU@users.noreply.github.com>
2025-09-16 18:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+const btn=document.getElementById("sampleTable")
 function insert_Row() {
+	const tr=document.createElement("tr")
+    const td1=document.createElement("td")
+	td1.textContent="New Cell1"
+
+
+    const td2=document.createElement("td")
+	td2.textContent="New Cell2"
+
+	tr.append(td1,td2)
+	btn.prepend(tr)
     //Write your code here
   
   
